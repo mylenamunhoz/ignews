1334cf4868a0bb4c8093b9fef2f442fd65ec2c5d
mylena.prado <mylena.prado@edu.unipar.br>
2022-03-22 17:57:20 -0300

    add link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router'
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
